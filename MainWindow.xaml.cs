323147024c501f0c8d76f3673ce7b7dbb82279d4
ShixinTang <shixin.tang@outlook.com>
2024-02-23 00:39:59 -0500

    Update Main Window features

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LLMS.View;
+using LLMS.ViewModel;
 
 namespace LLMS
 {
@@ -22,14 +23,27 @@ namespace LLMS
     public partial class MainWindow : Window
     {
         // Define LeaseWindow object
-        private LeaseWindow leaseWindow;
+        //private LeaseWindow leaseWindow;
+
+        // Define AzureDbContext object
+        private testdb1Entities db = new testdb1Entities();
 
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += Window_Loaded;
+
             // Initialize LeaseWindow object
-            leaseWindow = new LeaseWindow();
+            //leaseWindow = new LeaseWindow();
         }
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //load all lease information from the database
+            LoadMainWindow();
+        }
+
         private void Save_Click(object sender, RoutedEventArgs e)
         {
             // Handle Save button click
@@ -41,7 +55,7 @@ namespace LLMS
             Close();
         }
 
-        private void OpenLeaseWindow_Click(object sender, RoutedEventArgs e)
+        /*private void OpenLeaseWindow_Click(object sender, RoutedEventArgs e)
         {
             // Hide main window content
             this.Visibility = Visibility.Collapsed;
@@ -49,36 +63,84 @@ namespace LLMS
             leaseWindow.ShowDialog();
             // Show main window content again when the lease window is closed
             this.Visibility = Visibility.Visible;
-        }
+        }*/
 
-        private void OpenTenantWindow_Click(object sender, RoutedEventArgs e)
+        /*private void OpenTenantWindow_Click(object sender, RoutedEventArgs e)
         {
             // Handle opening Tenant window
-        }
+        }*/
 
-        private void OpenPropertyWindow_Click(object sender, RoutedEventArgs e)
+        /*private void OpenPropertyWindow_Click(object sender, RoutedEventArgs e)
         {
             // Handle opening Property window
-        }
+        }*/
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Handle selection change in ListView
+            if (listView.SelectedItem is MainWindowViewModel selectedLease)
+            {
+                // update right panel with selected lease information
+                UpdateRightPanel(selectedLease);
+            }
         }
 
         private void LeaseDetail_Click(object sender, RoutedEventArgs e)
         {
             // Handle Lease Detail button click
+            LeaseWindow leaseWindow = new LeaseWindow();
+            leaseWindow.ShowDialog();
         }
 
         private void TenantDetail_Click(object sender, RoutedEventArgs e)
         {
             // Handle Tenant Detail button click
+            //TenantWindow tenantWindow = new TenantWindow();
+            //tenantWindow.ShowDialog();
         }
 
         private void PropertyDetail_Click(object sender, RoutedEventArgs e)
         {
             // Handle Property Detail button click
+            //PropertyWindow propertyWindow = new PropertyWindow();
+            //propertyWindow.ShowDialog();
         }
+
+        private void LoadMainWindow()
+        {
+            var MainWindowViewModels = db.leases.Select(lease => new MainWindowViewModel
+            {
+                LeaseId = lease.id,
+                PaymentDueDay = lease.payment_due_day,
+                RentAmount = lease.rent_amount,
+                Address = lease.property.address,
+                TenantName = lease.tenant.first_name + " " + (lease.tenant.last_name ?? ""), // tenantname is first name + last name
+                PhoneNo = lease.tenant.phone_number,
+                Email = lease.tenant.email,
+                ImageUrl = lease.property.image.image_url, // image_url: url of the image store in Azure blob Storage
+                EmergencyContactName = lease.tenant.emergency_contact_name,
+                EmergencyContactNo = lease.tenant.emergency_contact_number
+            }).ToList();
+
+            listView.ItemsSource = MainWindowViewModels;
+
+            // default select the first item in the list
+            if (listView.Items.Count > 0)
+            {
+                listView.SelectedItem = MainWindowViewModels[0];
+            }
+        }
+
+        private void UpdateRightPanel(MainWindowViewModel lease)
+        {
+            lblAddress.Content = lease.Address;
+            lblTenantName.Content = lease.TenantName;
+            lblPhoneNo.Content = lease.PhoneNo;
+            lblEmail.Content = lease.Email;
+            lblEmergencyContactName.Content = lease.EmergencyContactName;
+            lblEmergencyContactNo.Content = lease.EmergencyContactNo;
+            //update image URL
+            imageDisplay.Source = new BitmapImage(new Uri(lease.ImageUrl, UriKind.Absolute));
+        }
+
     }
 }
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLMS.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public int LeaseId { get; set; }
+        public int PaymentDueDay { get; set; }
+        public decimal RentAmount { get; set; }
+        public string Address { get; set; }
+        public string TenantName { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactNo { get; set; }
+    }
+}
