8a9923f9e00672f6cab1b287ba49ef3bcf8e6040
ShixinTang <shixin.tang@outlook.com>
2024-02-25 18:05:30 -0500

    update the way to get environment

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,16 +4,44 @@ using System;
 using System.Windows;
 using Unity;
 using Unity.Injection;
+using dotenv.net;
+using System.Reflection;
+using System.IO;
 
 namespace LLMS
 {
     public partial class App : Application
     {
         private IUnityContainer _container = new UnityContainer();
+        private string blobconnectionString;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            // get the complete path of the .exe file
+            string exePath = Assembly.GetExecutingAssembly().Location;
+
+            // get the directory of the .exe file
+            string exeDirectory = Path.GetDirectoryName(exePath);
+
+            //MessageBox.Show($"exeDirectory: {exeDirectory}", "exe file path", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            // suppose the .env file is in the two lever parent directory of the .exe file
+            string envFilePath = Path.Combine(exeDirectory, "..", "..", ".env");
+
+            // convert the relative path to the absolute path
+            string envFileAbsolutePath = Path.GetFullPath(envFilePath);
+
+            //MessageBox.Show($"envFileAbsolutePath: {envFileAbsolutePath}", ".env file path", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            // use the absolute path to load the .env file
+            DotEnv.Load(new DotEnvOptions(envFilePaths: new[] { envFileAbsolutePath }));
+
+            // get the environment variable, copy the following code to the place where you want to use the environment variable
+            blobconnectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            //string containerName = Environment.GetEnvironmentVariable("NameContainer");
+
 
             _container = new UnityContainer();
             ConfigureContainer();
@@ -24,7 +52,8 @@ namespace LLMS
 
         private void ConfigureContainer()
         {
-            var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            //var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            var connectionString = blobconnectionString;
             if (string.IsNullOrEmpty(connectionString))
             {
                 throw new InvalidOperationException("Azure Storage connection String is not set.");
