ff7b3f341b41dafdec13d8d2ff86641f19e20f36
CAO LISI <137085653+lisiCAO@users.noreply.github.com>
2024-02-27 22:33:53 -0500

    Update IAzureBlobStorageClient.cs

--- Service/IAzureBlobStorageClient.cs
+++ Service/IAzureBlobStorageClient.cs
@@ -10,7 +10,7 @@ namespace LLMS.Service
     public interface IAzureBlobStorageClient
     {
         Task<string> UploadFileAsync(string containerName, string blobName, Stream fileStream);
-        // 定义接口中的其他方法...
+        // Other Methods
     }
 
 }
